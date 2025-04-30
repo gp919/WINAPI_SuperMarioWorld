fea708d2ea340816533836d4018145795848ab3e
gp919 <asbssu18@gmail.com>
2025-04-30 16:14:32 +0900

    update : 캐릭터 이미지

---
+++ SuperMarioWorld/SuperMarioWorld/CMyBmp.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "CMyBmp.h"
+
+CMyBmp::CMyBmp()
+{
+}
+
+CMyBmp::~CMyBmp()
+{
+	Release();
+}
+
+void CMyBmp::Load_Bmp(const TCHAR* pFilePath)
+{
+	HDC	hDC = GetDC(g_hWnd);	// 화면 DC (직접 그리는 것)
+
+	m_hMemDC = CreateCompatibleDC(hDC);	// 메모리 DC (오프스크린 작업용)
+
+	ReleaseDC(g_hWnd, hDC);
+
+	// 이미지 파일에서 비트맵 객체 생성
+	m_hBitmap = (HBITMAP)LoadImage(NULL,		// 인스턴스 핸들(파일로부터 읽어올 것이기 때문에 null)
+		pFilePath,	// 파일의 경로
+		IMAGE_BITMAP, // 어떤 타입을 읽어올 것인가(이미지 파일)
+		500, 1000, // 이미지 가로, 세로 사이즈
+		LR_LOADFROMFILE | LR_CREATEDIBSECTION); // LR_LOADFROMFILE : 파일로부터 이미지를 읽어들임, LR_CREATEDIBSECTION : 읽어온 파일을 dib 형태로 변환
+
+	m_hOldBmp = (HBITMAP)SelectObject(m_hMemDC, m_hBitmap);
+
+	// ... hMemDC로 그림 작업 수행 ...
+
+}
+
+void CMyBmp::Release()
+{
+	// SelectObject : gdi 오브젝트를 선택하기 전에 기존에 가지고 있던 오브젝트를 반환
+	SelectObject(m_hMemDC, m_hOldBmp);
+	DeleteObject(m_hBitmap);
+	DeleteDC(m_hMemDC);
+}
--- SuperMarioWorld/SuperMarioWorld/CPlayer.cpp
+++ SuperMarioWorld/SuperMarioWorld/CPlayer.cpp
@@ -13,24 +13,57 @@ CPlayer::~CPlayer()
 
 void CPlayer::Initialize()
 {
-	m_tInfo = { 8.f, WINCY-8.f, 16.f, 16.f };
+	m_tInfo = { TILECX / 2.f + 0.1f, WINCY - (TILECY / 2.f + 0.1f), TILECX, TILECY };
+	m_fSpeed = 10.f;
+	CBmpMgr::Get_Instance()->Insert_Bmp(L"../Resource/Player/Mario_LMove.bmp", L"Player_LEFT");
+	CBmpMgr::Get_Instance()->Insert_Bmp(L"../Resource/Player/Mario_RMove.bmp", L"Player_RIGHT");
+
+	m_eDir = DIR_RIGHT;
+	m_pFrameKey = L"Player_RIGHT";
+
+	m_tFrame.iStart = 0;
+	m_tFrame.iEnd = 3;
+	m_tFrame.iMotion = 0;
+	m_tFrame.dwSpeed = 200;
+	m_tFrame.dwTime = GetTickCount();
+
+
 }
 
 int CPlayer::Update()
 {
 	Key_Input();
+	Change_Motion();
+	CObject::Move_Frame();
 	CObject::Update_Rect();
 	
-	return 0;
+	return NOEVENT;
 }
 
 void CPlayer::Late_Update()
 {
+	
 }
 
 void CPlayer::Render(HDC hDC)
 {
 	Rectangle(hDC, m_tInfo.fX - (m_tInfo.fCX / 2), m_tInfo.fY - (m_tInfo.fCY / 2), m_tInfo.fX + (m_tInfo.fCX / 2), m_tInfo.fY + (m_tInfo.fCY / 2));
+
+	HDC hMemDC = CBmpMgr::Get_Instance()->Find_Image(m_pFrameKey);
+
+	GdiTransparentBlt(hDC,
+		m_tRect.left,
+		m_tRect.top,
+		(int)m_tInfo.fCX,
+		(int)m_tInfo.fCY,
+		hMemDC,
+		// 복사할 이미지의 비트맵 시작 좌표
+		m_tFrame.iStart * (int)m_tInfo.fCX,
+		m_tFrame.iMotion * (int)m_tInfo.fCY,	
+		// 복사할 이미지의 가로, 세로 사이즈
+		(int)m_tInfo.fCX,	
+		(int)m_tInfo.fCY,
+		RGB(0, 255, 0));
 }
 
 void CPlayer::Release()
@@ -39,15 +72,20 @@ void CPlayer::Release()
 
 void CPlayer::Key_Input()
 {
+
 	// 이동 : 좌우
 	if (GetAsyncKeyState(VK_LEFT))
 	{
 		m_tInfo.fX -= m_fSpeed;
+		m_eDir = DIR_LEFT;
+		m_eCurState = WALK;
 	}
 
 	else if (GetAsyncKeyState(VK_RIGHT))
 	{
 		m_tInfo.fX += m_fSpeed;
+		m_eDir = DIR_RIGHT;
+		m_eCurState = WALK;
 	}
 
 	// 올려보기, 웅크리기
@@ -77,4 +115,66 @@ void CPlayer::Key_Input()
 
 	}
 
+}
+
+void CPlayer::Change_Motion()
+{
+	if (m_ePreState != m_eCurState)
+	{
+		switch (m_eCurState)
+		{
+		case IDLE:
+			m_tFrame.iStart = 0;
+			m_tFrame.iEnd = 0;
+			m_tFrame.iMotion = 0;
+			m_tFrame.dwSpeed = 200;
+			m_tFrame.dwTime = GetTickCount();
+			break;
+
+		case WALK:
+			m_tFrame.iStart = 0;
+			m_tFrame.iEnd = 1;
+			m_tFrame.iMotion = 1;
+			m_tFrame.dwSpeed = 200;
+			m_tFrame.dwTime = GetTickCount();
+			break;
+
+		case RUN:
+			m_tFrame.iStart = 0;
+			m_tFrame.iEnd = 1;
+			m_tFrame.iMotion = 2;
+			m_tFrame.dwSpeed = 200;
+			m_tFrame.dwTime = GetTickCount();
+			break;
+
+		case JUMP:
+			break;
+
+		case JUMP_ACCEL:
+			break;
+
+		case STOMP:
+			break;
+		case CROUCH:
+			break;
+		case BRAKE:
+			break;
+		case KICK:
+			break;
+		case HOLD_IDLE:
+			break;
+		case HOLD_MOVE:
+			break;
+		case PDEAD:
+			break;
+		}
+		m_ePreState = m_eCurState;
+
+		if(m_eDir=DIR_RIGHT)
+			m_pFrameKey = L"Player_RIGHT";
+		else if(m_eDir = DIR_LEFT)
+			m_pFrameKey = L"Player_LEFT";
+	}
+
+	
 }
